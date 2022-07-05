46e6bccc675e9c424f204ef0f4b275a518aa91bb
melindachhu <chhumelinda@gmail.com>
2022-07-05 09:27:15 -0500

    M2

--- module2/budget_2.go
+++ module2/budget_2.go
@@ -53,7 +53,12 @@ var errDuplicateEntry = errors.New("Cannot add duplicate entry")
 
 // AddItem adds an item to the current budget
 func (b *Budget) AddItem(description string, price float32) error {
+	if b.CurrentCost()+price > b.Max {
+		return errDoesNotFitBudget
+	}
 
+	newItem := Item{Description: description, Price: price}
+	b.Items = append(b.Items, newItem)
 	return nil
 }
 
@@ -61,7 +66,8 @@ func (b *Budget) AddItem(description string, price float32) error {
 func (b *Budget) RemoveItem(description string) {
 	for i := range b.Items {
 		if b.Items[i].Description == description {
-
+			b.Items = append(b.Items[:i], b.Items[i+1:]...)
+			break
 		}
 	}
 }
@@ -69,6 +75,22 @@ func (b *Budget) RemoveItem(description string) {
 // CreateBudget creates a new budget with a specified max
 func CreateBudget(month time.Month, max float32) (*Budget, error) {
 	var newBudget *Budget
+	tempBudget := Budget{Max: max}
+	newBudget = &tempBudget
+
+	// checks to see if the report is full
+	if len(report) >= 12 {
+		return nil, errReportIsFull
+	}
+
+	// checks to see if the month already exists
+	_, hasEntry := report[month]
+	if hasEntry {
+		return nil, errDuplicateEntry
+	}
+
+	// adds the month to the report
+	report[month] = newBudget
 
 	return newBudget, nil
 }
@@ -76,7 +98,12 @@ func CreateBudget(month time.Month, max float32) (*Budget, error) {
 // GetBudget returns budget for given month
 func GetBudget(month time.Month) *Budget {
 
-	return nil
+	budget, hasEntry := report[month]
+	if hasEntry {
+		return budget
+	} else {
+		return nil
+	}
 }
 
 // END Project code
